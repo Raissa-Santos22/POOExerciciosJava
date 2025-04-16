05bf9f586419a7645d1a5380eaf7854e7055144c
Raissa Santos22 <raissa.santos171@etec.sp.gov.br>
2025-04-16 20:03:09 -0300

    5 exercícios sobre Método com Retorno, uso do return, porém, sem parametros

---
+++ src/MetodoComRetorno.java
@@ -0,0 +1,34 @@
+
+public class MetodoComRetorno {
+    public static void main(String[] args) {
+        System.out.println("Ano atual: " + MetodoComRetorno.exibir()); // não tem parametros
+        System.out.println("Nome completo : " + MetodoComRetorno.retornar());
+        System.out.println(MetodoComRetorno.numerar());
+        System.out.println(MetodoComRetorno.mostrar());
+        System.out.println("O numero de pi é : " + MetodoComRetorno.retornarPi());
+    }
+    public static int exibir(){
+        int ano = 2025;
+        return ano; // return usado em metodos com retorno
+    }
+    public static String retornar(){
+        String nomeCompleto = "Raissa Amorim dos Santos";
+        return nomeCompleto;
+    }
+    public static int numerar(){
+       int numeroFixo = 100;
+        return numeroFixo;
+    }
+    public static boolean mostrar(){
+        boolean sim = true; // booleano que só é usado para verdadeiro ou falso
+        return sim;
+    }
+    public static float retornarPi(){
+        double pi = 3.144159;
+        return(float) pi;
+    }
+
+
+
+
+}
